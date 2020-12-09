8260d7d67f033390f91c562f9b79d28c4c98aea6
CrystalVulpine <crystalvulpine@gmail.com>
2020-12-09 12:08:27 -0500

    Banned users should show up in the mod list so they can be kicked

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -72,8 +72,7 @@ class Board(Base, Stndrd, Age_times):
     @property
     def mods_list(self):
 
-        z = [x for x in self.moderators if x.accepted and not (
-            x.user.is_deleted or (x.user.is_banned and not x.user.unban_utc))]
+        z = [x for x in self.moderators if x.accepted and not x.user.is_deleted]
 
         z = sorted(z, key=lambda x: x.id)
         return z
